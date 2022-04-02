4b70eefc758f7520369f69182fa73e985aaa2d5c
MuhammadBilal <muhammadbilal20130@gmail.com>
2022-04-02 09:27:08 +0500

    Login and login functionality added

--- app/src/main/java/com/example/emaillogintesting/MainActivity.java
+++ app/src/main/java/com/example/emaillogintesting/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     Button Btn_login,Btn_register;
+    EditText ET_email,ET_password;
+    String email,password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,14 +32,55 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         Btn_login=findViewById(R.id.Btn_login);
         Btn_register=findViewById(R.id.Btn_register);
+        ET_email=findViewById(R.id.ET_email);
+        ET_password=findViewById(R.id.ET_password);
 
+
+
+    //for registering new user
         Btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this,RegisterActivity.class));
             }
         });
+        // for sign in existing user
+        Btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                email=ET_email.getText().toString();
+                password=ET_password.getText().toString();
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Enter Credentials", Toast.LENGTH_SHORT).show();
+                }else{
+                    singIn_with_firebase(email,password);
+                }
 
+            }
+        });
+
+
+    }
+
+    private void singIn_with_firebase(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG for success", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(MainActivity.this,WokingActivity.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG for Error", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 //    public void onStart() {
 //        super.onStart();
@@ -46,5 +90,6 @@ public class MainActivity extends AppCompatActivity {
 //            Toast.makeText(this, "Already sign in", Toast.LENGTH_LONG).show();
 //        }
 //    }
+
 
 }
---
+++ app/src/main/java/com/example/emaillogintesting/WokingActivity.java
@@ -0,0 +1,31 @@
+package com.example.emaillogintesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class WokingActivity extends AppCompatActivity {
+
+    Button logout;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_woking);
+
+        logout=findViewById(R.id.Btn_W_logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.getInstance().signOut();
+                startActivity(new Intent(WokingActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
